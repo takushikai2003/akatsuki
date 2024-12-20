0b99d50bfda15fdfe3ca9fa057362fb631a57df3
nasuunasuu <nasuu0612@gmail.com>
2024-12-20 13:06:28 +0900

    fx[名前が紛らわしいため変更]

---
+++ data/loginCards.js
@@ -0,0 +1,8 @@
+// ログインボーナス時にここからCardデータを取得して表示させる
+/**
+ * @type {Card[]} loginCardsData 
+ */
+export const loginCardsData = [
+    { id: 1, name: "おにぎり", image: "../guzai_images/onigiri.png", score: 130, category: "rice" },
+    { id: 3, name: "とまと", image: "../guzai_images/tomato.png", score: 100, category: "vegetable" },
+];
