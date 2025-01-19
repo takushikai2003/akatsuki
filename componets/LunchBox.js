b0ec322e3564bc4bd401f5a82046c6887523d827
nasuunasuu <nasuu0612@gmail.com>
2025-01-19 10:13:37 +0900

    [fix]canvasタグに変更

--- componets/LunchBox.js
+++ componets/LunchBox.js
@@ -2,7 +2,6 @@ import { getCardDataById } from "../data/cards.js";
 import { getGuzaiInLunchBox } from "../lib/lunchBox.js";
 
 const lunchBox = document.getElementById("lunchBox");
-const LBcontents = document.getElementById("LBcontents");
 
 const speed = 7; //移動速度
 const lunchBoxStyle = getComputedStyle(lunchBox);
@@ -19,29 +18,18 @@ export function downLunchBox(){
 export function showLunchBox() {
     const GuzaisInLunchBox = getGuzaiInLunchBox();//弁当箱に入っている具材のidの配列
     let guzaiPlaceNumber=0;
+    const w=window.innerWidth;
+    const h=w*235/547;
 
-    lunchBox.innerHTML=`
-    <img id="LBback" src="../componets/images_lunchbox/lunchBox_back.png">
-      <div id="LBcontents">
-        <!--この間に具材を入れると弁当箱に入る-->
-        <img src="../guzai_images/onigiri.png">
-      </div>
-      <img id="LBfront" src="../componets/images_lunchbox/lunchBox_front.png">
-    `;
+    lunchBox.innerHTML=`<canvas id="lunchBoxCanvas" width=${w} height=${h}></canvas>`;
 
-    /*
-    for (const guzaiId of GuzaisInLunchBox) {
-        //仕切られた空間の中に具材が入っていたら、表示
-        if (guzaiId) {
-            let guzaiData = getCardDataById(guzaiId); //具材のidから具材のデータを持って来る
-            let guzaiImage=guzaiData.image;
+    const canvas = document.getElementById("lunchBoxCanvas");
+    const ctx = canvas.getContext("2d");
 
-            LBcontents.innerHTML=`
-                <img id="guzaiImage_${guzaiPlaceNumber}" src="${guzaiImage}">
-            `;
-
-        }
-        guzaiPlaceNumber++;
+    const img = new Image();
+    img.src = "../componets/images_lunchbox/lunchbox_back.png";
+    img.onload = function(){
+        ctx.drawImage(img, 0, 0, w, h);
     }
-        */
+        
 }
