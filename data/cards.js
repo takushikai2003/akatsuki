504484b2cc2f0f356114f1319760b881f56ca25d
takushikai <takushikai@gmail.com>
2025-01-23 01:15:06 +0900

    change images

--- data/cards.js
+++ data/cards.js
@@ -4,14 +4,14 @@
  * @type {Card[]} cardsData 
  */
 export const cardsData = [
-    { id: 1, name: "おにぎり", image: "../guzai_images/onigiri_nori.jpg", score: 130, category: "rice" },
-    { id: 2, name: "おにぎり", image: "../guzai_images/onigiri_furikake.jpg", score: 130, category: "rice" },
-    { id: 3, name: "とまと", image: "../guzai_images/tomato.jpg", score: 100, category: "vegetable" },
-    { id: 4, name: "たまご", image: "../guzai_images/egg.jpg", score: 100, category: "vegetable" },
-    { id: 5, name: "えだまめ", image: "../guzai_images/edamame.jpg", score: 100, category: "vegetable", },
-    { id: 6, name: "ミートボール", image: "../guzai_images/meatball.jpg", score: 100, category: "vegetable" },
-    { id: 7, name: "ポテトサラダ", image: "../guzai_images/potatosalad.jpg", score: 100, category: "vegetable" },
-    { id: 8, name: "タコさん", image: "../guzai_images/takosan.jpg", score: 100, category: "vegetable" },
+    { id: 1, name: "おにぎり", image: "../guzai_images/onigiri_nori.png", score: 130, category: "rice" },
+    { id: 2, name: "おにぎり", image: "../guzai_images/onigiri_furikake.png", score: 130, category: "rice" },
+    { id: 3, name: "とまと", image: "../guzai_images/tomato.png", score: 100, category: "vegetable" },
+    { id: 4, name: "たまご", image: "../guzai_images/egg.png", score: 100, category: "vegetable" },
+    { id: 5, name: "えだまめ", image: "../guzai_images/edamame.png", score: 100, category: "vegetable", },
+    { id: 6, name: "ミートボール", image: "../guzai_images/meatball.png", score: 100, category: "vegetable" },
+    { id: 7, name: "ポテトサラダ", image: "../guzai_images/potatosalad.png", score: 100, category: "vegetable" },
+    { id: 8, name: "タコさん", image: "../guzai_images/takosan.png", score: 100, category: "vegetable" },
     { id: 9, name: "空き缶", image: "../guzai_images/akikan.png", score: -100, category: "trash" },
     { id: 10, name: "長靴", image: "../guzai_images/nagagutu.png", score: -50, category: "trash" },
 ];
